3d8a5a59da2da59a95de52ac4797c3760cd0fafc
José Lucas Silva Mayer <josemayer@usp.br>
2021-12-26 15:33:06 -0400

    Corrigir layout e adicionar novos componentes na barra de navegações
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,11 +1,20 @@
 <template>
   <v-card rounded="0">
     <v-toolbar app color="#A51515" class="white--text hidden-md-and-down">
+      <v-spacer />
       <NuxtLink class="atalho white--text" to="/historia">História</NuxtLink>
 
-      <v-menu offset-y open-on-hover>
+      <v-menu
+        rounded="0"
+        content-class="elevation-0"
+        transition="slide-y-transition"
+        offset-y
+        open-on-hover
+      >
         <template v-slot:activator="{ on, attrs }">
-          <div class="atalho" v-bind="attrs" v-on="on">Gestão ▾</div>
+          <div class="atalho" v-bind="attrs" v-on="on">
+            Gestão <v-icon small color="white">mdi-menu-down</v-icon>
+          </div>
         </template>
         <v-list color="#A51515">
           <v-list-item v-for="(item, index) in itemsGestao" :key="index" link>
@@ -19,9 +28,20 @@
       </v-menu>
 
       <NuxtLink class="atalho white--text" to="/lojinha">Lojinha</NuxtLink>
-      <v-menu offset-y open-on-hover>
+      <NuxtLink class="atalho white--text" to="/">
+        <v-img src="/logo.svg" position="center top" class="logo"></v-img>
+      </NuxtLink>
+      <v-menu
+        rounded="0"
+        content-class="elevation-0"
+        transition="slide-y-transition"
+        offset-y
+        open-on-hover
+      >
         <template v-slot:activator="{ on, attrs }">
-          <div class="atalho" v-bind="attrs" v-on="on">Esportivo ▾</div>
+          <div class="atalho" v-bind="attrs" v-on="on">
+            Esportivo <v-icon small color="white">mdi-menu-down</v-icon>
+          </div>
         </template>
         <v-list color="#A51515">
           <v-list-item
@@ -39,6 +59,7 @@
       </v-menu>
       <NuxtLink class="atalho white--text" to="/eventos">Eventos</NuxtLink>
       <NuxtLink class="atalho white--text" to="/contato">Contato</NuxtLink>
+      <v-spacer />
     </v-toolbar>
   </v-card>
 </template>
@@ -60,8 +81,11 @@ export default {
 
 <style scoped>
 .atalho {
-  padding: 20px 70px;
+  padding: 20px 50px;
   text-decoration: none;
+}
+.logo {
+  margin-top: -170px;
 }
 .item {
   text-decoration: none;
